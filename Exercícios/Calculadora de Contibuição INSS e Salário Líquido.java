b476bf4bb272debf7f3d31814a5fccf8360e0654
José Felipe <100253391+JFMGDB@users.noreply.github.com>
2024-09-10 14:14:32 -0300

    Update Calculadora de Contibuição INSS e Salário Líquido.java

--- Exercícios/Calculadora de Contibuição INSS e Salário Líquido.java
+++ Exercícios/Calculadora de Contibuição INSS e Salário Líquido.java
@@ -1,13 +1,3 @@
-# Calculadora de Contribuição ao INSS e Salário Líquido
-
- - Peça ao usuário para inserir seu salário bruto. Calcule o valor de contribuição ao INSS com base nas alíquotas vigentes. Exiba o valor da contribuição e o salário líquido.
-   
-**Resolução:**
-
-<details>
-  <summary>Spoiler warning</summary>
-
-```
 import java.util.Scanner;
 
 public class Main {
@@ -20,48 +10,30 @@ public class Main {
       System.out.println("Digite seu salario bruto:");
       salarioBruto = read.nextDouble();
 
-
       if(salarioBruto <= 1320.00) {
-        
+          
         inss = salarioBruto * 0.075;
-        double salarioLiquido = salarioBruto - inss;
-        
-          System.out.println("Sua contribuicao para o INSS em r$ é: " + inss);
-          System.out.println("Seu salario liquido em r$ é: " + salarioLiquido);
       }
       else if(salarioBruto <=2571.29) {
-        
+          
         inss =  (1320.00 * 0.075) + ((salarioBruto - 1320.00) * 0.09) ;
-        double salarioLiquido = salarioBruto - inss;
-        
-          System.out.println("Sua contribuicao para o INSS em r$ é: " + inss);
-          System.out.println("Seu salario liquido em r$ é: " + salarioLiquido);
       }
       else if(salarioBruto <= 3856.94) {
-        
+          
         inss = (1320.00 * 0.075) + ((2571.29 - 1320.00) * 0.09) + ((salarioBruto - 2571.29) * 0.12);
-        double salarioLiquido = salarioBruto - inss;
-        
-          System.out.println("Sua contribuicao para o INSS em r$ é: " + inss);
-          System.out.println("Seu salario liquido em r$ é: " + salarioLiquido);
       }
       else if(salarioBruto <= 7507.49) {
-        
+          
         inss = (1320.00 * 0.075) + ((2571.29 - 1320.00) * 0.09) + ((3856.94 - 2571.29) * 0.12) + ((salarioBruto - 3856.94) * 0.14);
-        double salarioLiquido = salarioBruto - inss;
-        
-          System.out.println("Sua contribuicao para o INSS em r$ é: " + inss);
-          System.out.println("Seu salario liquido em r$ é: " + salarioLiquido);
       }
       else{
-        
+
         inss= 877.24;
-        double salarioLiquido = salarioBruto - inss;
-        
-          System.out.println("Sua contribuicao para o INSS em r$ é: " + inss);
-          System.out.println("Seu salario liquido em r$ é: " + salarioLiquido);
       }
+
+        double salarioLiquido = salarioBruto - inss;
+
+        System.out.println("Sua contribuicao para o INSS em R$ é: " + inss);
+        System.out.println("Seu salario liquido em R$ é: " + salarioLiquido);
    }
 }
-```
-</details>
